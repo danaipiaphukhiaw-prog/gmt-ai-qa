c8c155592d8fa56f54fcf833f3f84e51f1a1e741
danaipiaphukhiaw-prog <danai.piaphukhiaw@gmail.com>
2026-09-07 11:05:52 +0700

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ handler = WebhookHandler(os.getenv("LINE_CHANNEL_SECRET"))
 
 # Gemini setup
 genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
-model = genai.GenerativeModel("gemini-1.5-flash")
+model = genai.GenerativeModel("gemini-1.5-flash-latest")
 
 @app.route("/callback", methods=['POST'])
 def callback():
